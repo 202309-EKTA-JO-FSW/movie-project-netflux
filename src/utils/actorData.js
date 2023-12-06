e67f6b018fcb136d0613232ff88b7119605184a1
Sanad-Alshobaki <alshobakisanad@gmail.com>
2023-12-06 16:17:01 +0300

    fix errors

--- src/utils/actorData.js
+++ src/utils/actorData.js
@@ -1,26 +1,41 @@
 import { defaultImage } from "./defaultImage"
 
-
 const TMDB_API_KEY = "1e3ba2c2144bfdc63d5615205918151c"
-
-const TMDB_API_BASE_URL = 'https://api.themoviedb.org/3';
+const TMDB_API_BASE_URL = "https://api.themoviedb.org/3"
 
 export async function getActorDetails(actorId) {
+  try {
+    // Fetch actor details
     const actorResponse = await fetch(
-        `https://api.themoviedb.org/3/person/${actorId}?api_key=${TMDB_API_KEY}&language=en-US`,
+      `${TMDB_API_BASE_URL}/person/${actorId}?api_key=${TMDB_API_KEY}&language=en-US`,
     )
 
     if (!actorResponse.ok) {
-        throw new Error("Failed to fetch movie details")
+      throw new Error("Failed to fetch actor details")
     }
 
-    const movieData = await actorResponse.json()
+    const actorData = await actorResponse.json()
 
+    // Fetch movies for the actor
+    const moviesResponse = await fetch(
+      `${TMDB_API_BASE_URL}/person/${actorId}/movie_credits?api_key=${TMDB_API_KEY}&language=en-US`,
+    )
+
+    if (!moviesResponse.ok) {
+      throw new Error("Failed to fetch actor movies")
+    }
+
+    const moviesData = await moviesResponse.json()
+
+    // Combine actor details with the list of movies
     const actorDetails = {
-        ...movieData,
-
+      ...actorData,
+      movies: moviesData.cast, // Assuming the cast property contains the list of movies
     }
 
     return actorDetails
+  } catch (error) {
+    console.error("Error fetching actor details:", error.message)
+    throw new Error("Failed to fetch actor details")
+  }
 }
-
